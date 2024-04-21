d9933e47f77134b093513237c2bf520a7bea7e70
muddyrain <qiu3291002845@gmail.com>
2024-04-21 18:44:21 +0800

    20

---
+++ 20.二叉树的中序遍历.js
@@ -0,0 +1,35 @@
+/**
+ * 二叉树的前序遍历
+ */
+
+function Node(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+var a = new Node("A");
+var b = new Node("B");
+var c = new Node("C");
+var d = new Node("D");
+var e = new Node("E");
+var f = new Node("F");
+var g = new Node("G");
+
+a.left = c;
+a.right = b;
+
+c.left = f;
+c.right = g;
+
+b.left = d;
+b.right = e;
+
+function f1(root) {
+  if (root === null) return;
+  f1(root.left);
+  console.log(root.value);
+  f1(root.right);
+}
+
+f1(a);
